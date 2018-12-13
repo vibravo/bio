840b21d5c789c2bb8ea2ca3f312e38ed1e5a8327
vibravo <45087542+vibravo@users.noreply.github.com>
2018-12-13 12:15:50 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -4,12 +4,12 @@ function preload() {
 }
 function setup() {
 	createCanvas(400, 600);
-	headshot.resize(100,0);
+	headshot.resize(150,0);
 }
 
 function draw() {
 	noStroke();
 	fill(241,213,204);
 	rect(0,120,400,480);
-	image(headshot,105,100,100,100);
+	image(headshot,105,100,150,150);
 }
